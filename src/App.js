5a33e0d42c8c9a949f2d90d09cf48b23fc6ead90
fruxc <hammadansari17@gmail.com>
2020-04-11 15:35:58 +0530

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
-import Menu from './component/MenuComponent';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
+import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Confusion Restaurant</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Confusion Restaurant</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
