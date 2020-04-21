f6cd51d5fd8f58b6554a9715e87891782eba5e9a
fruxc <hammadansari17@gmail.com>
2020-04-21 23:51:57 +0530

    React Redux Forms Revisited

---
+++ src/components/LoadingComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Loading = () => {
+  return (
+    <div className="col-12 text-center">
+      <span className="fa fa-circle-o-notch fa-pulse fa-3x fa-fw text-success"></span>
+    </div>
+  );
+};
